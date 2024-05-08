28a3d1b3cdd295a9476c3689981022b652fefdc6
Phạm Thanh Tùng <tungpham010203@gmail.com>
2024-05-09 04:22:47 +0700

    first commit

--- app/src/main/java/com/example/thaphuong/HomeFragment.java
+++ app/src/main/java/com/example/thaphuong/HomeFragment.java
@@ -9,6 +9,13 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Spinner;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 
 public class HomeFragment extends Fragment {
     Spinner spinner_university;
@@ -22,5 +29,27 @@ public class HomeFragment extends Fragment {
         spinner_university = view.findViewById(R.id.spinner_university);
     }
     void addEvents(View view) {
+        view.findViewById(R.id.buttonKhan).setOnClickListener(v -> {
+            String content = ((Spinner) view.findViewById(R.id.spinner_university)).getSelectedItem().toString();
+            String uneversity = "Đại học Kinh tế";
+            addItemToFirebase(content, uneversity);
+        });
+    }
+    private void addItemToFirebase(String content, String uneversity) {
+        Locale vietnam = new Locale("vi", "VN");
+
+        SimpleDateFormat day = new SimpleDateFormat("ddMMyyyy", vietnam);
+        String dayFormat = day.format(new Date());
+
+        SimpleDateFormat time = new SimpleDateFormat("HHmmss", vietnam);
+        String timeFormat = time.format(new Date());
+
+        SimpleDateFormat sdf = new SimpleDateFormat("ddMMyyyyHHmmss", vietnam);
+        String currentDateTimeString = sdf.format(new Date());
+
+        DatabaseReference dbRef = FirebaseDatabase.getInstance().getReference();
+
+        Content itemPush = new Content(content, uneversity, dayFormat, timeFormat);
+        dbRef.child(currentDateTimeString).setValue(itemPush);
     }
 }
